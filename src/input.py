8790c0c4b44a8676c4dfc6ecda493f0bbc52368b
Robert Keersse <robert.keersse@gmail.com>
2012-01-03 14:31:47 +0100

    rename basisbewerkingen

--- src/input.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on Jan 2, 2012
-
-@author: robert
-'''
-x=input("geef een eerste getal: ")
-y=input("geef een tweede getal: ")
-z=x+y
-print "de som van deze twee getallen is:",z
--- src/string.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on Jan 1, 2012
-
-@author: robert
-'''
-test = ('mom','dad','fien','jos','ba')
-test2 = ('jos','fien')
-test3 = ('mom','dad')
-print test + test2 + test3
