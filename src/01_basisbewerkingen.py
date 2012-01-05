10b7a6477b2f17a080672d9613a50bab4dfd20b5
Robert Keersse <robert.keersse@gmail.com>
2012-01-05 20:17:57 +0100

    wiskundige_functies

--- src/01_basisbewerkingen.py
+++ src/01_basisbewerkingen.py
@@ -4,7 +4,7 @@ Created on Jan 3, 2012
 @author: robert
 '''
 print ('Basisbewerkingen met Python')
-print ('Optellen, Aftrekken, Vermeningvuldigen en Delen')
+print ('\nOptellen, Aftrekken, Vermeningvuldigen en Delen')
 x=8
 y=2
 print x,('+'),y, ('= '),x+y
@@ -21,7 +21,7 @@ print x,('/'),y, ('= '),x/y ,('-> JUIST')
 y=2.0
 print x,('/'),-y, ('= '),x/-y ,('-> JUIST')
 
-print ('Modulus')
+print ('\nModulus')
 # Het bekendste voorbeeld van modulair rekenen is de tijdrekening in uren, 
 # die modulo 12 of modulo 24 gaat. Zo is de tijd bijvoorbeeld 10 uur na 22 
 # uur gelijk aan 8 uur.
@@ -29,7 +29,16 @@ x=32
 y=24
 print x,('%'),y, ('= '),x%y
 
-print ('Machtverheffing')
+print ('\nMachtverheffing')
 x=8
 y=4
 print x,('**'),y, ('= '),x**y
+
+print ('\nComplexe getallen')
+a=complex(3,4)
+b=2+3j
+print ('complex getal: '),a
+print ('Reele waarde: '),a.real
+print ('Imaginaire deel: '),a.imag
+print ('Absolute waarde: '),abs(a)
+print ('Bewerkingen complexe getallen: \n'),a,('+'),b,('='),a+b
--- src/02_wiskundige_functies.py
+++ src/02_wiskundige_functies.py
@@ -3,12 +3,26 @@ Created on Jan 4, 2012
 
 @author: robert
 '''
-print ('Wiskundige functies met Python')
-print ('Complexe getallen')
-a=3+4j
-b=2+3j
-print ('complex getal: '),a
-print ('Reele waarde: '),a.real
-print ('Imaginaire deel: '),a.imag
-print ('Absolute waarde: '),abs(a)
-print ('Som van 2 complexe getallen: \n'),a,('+'),b,('='),a+b
+print ('Math Module - Wiskundige functies')
+
+import math
+
+x=4.0
+y=3.0
+z=math.pow(x,y)
+print ('\n---Wortels en Machten---')
+print ('Vierkantswortel van'),x,('= '), math.sqrt(x)
+print x,('tot de'),y,('de = '), z
+print y,('de wortel van'),z,('= '), math.pow(z,(1/y))
+
+print ('\n---Logaritmen---')
+print ('e = '), math.e
+print ('log van 1000 = '),math.log10(1000)
+
+print ('\n---Goniometrie---')
+print ('pi = '),math.pi
+print ('2 rad = '),math.degrees(math.pi*2),('graden')
+print ('sin 60 = '),math.sin(math.radians(60))
+print ('tan 60 = '),math.tan(math.pi/3)
+print ('acos 0.5 = '),math.acos(0.5),('rad')
+print ('acos 0.5 = '),math.degrees(math.acos(0.5)),('graden')
