371fe3c9a0f166e407802b0e449b477fdc76196e
Robert Keersse <robert.keersse@gmail.com>
2012-01-17 15:38:39 +0100

    oop

---
+++ src/10_oop.py
@@ -0,0 +1,31 @@
+'''
+Created on Jan 17, 2012
+
+@author: robert
+'''
+class groupClass:
+    group='PC2'
+    teacher='Robert'
+    def teacherMethode(self):
+        print self.teacher,'is leerkracht van groep',self.group
+        
+group1=groupClass()
+print '%s' % group1.group  
+print group1.teacherMethode() 
+
+class studentClass(groupClass):
+    def createName(self,name):
+        self.name=name
+    def displayName(self):
+        return self.name
+    def greetingName(self):
+        print 'hallo %s' % self.name
+        
+student1=studentClass()
+student2=studentClass()
+student1.createName('Raf')
+student1.age=21
+student2.createName('Niels')
+print student1.displayName() , str(student1.age)
+print student2.group,student2.greetingName()
+
